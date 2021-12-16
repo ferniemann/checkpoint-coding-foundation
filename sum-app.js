5b8e5532eca96339226dccbff36d8282c15eac59
ferniemann <hello@ferdinand-niemann.eu>
2021-12-16 12:51:44 +0100

    Solution: Sum App

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,20 @@
 class SumApp {
-  // Add you solution here
+  numbers = []
+  sum = 0
+  constructor(n) {
+    this.number = n
+  }
+
+  addNumber(n) {
+    this.numbers.push(n)
+  }
+
+  getSum() {
+    this.numbers.forEach(num => this.sum += num)
+    return this.sum
+  }
+
+  reset() {
+    this.numbers = []
+  }
 }
