09994a50899163ea46c3f51f0ee8b57d136700be
ferniemann <hello@ferdinand-niemann.eu>
2021-12-16 13:00:50 +0100

    Solution: Zip Strings

--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,17 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  const arrA = strA.split("")
+  const arrB = strB.split("")
+  const arrMerged = []
+
+  if (arrA.length > arrB.length) {
+    for (let i = 0; i < arrA.length; i++) {
+      arrMerged.push(arrA[i], arrB[i])
+    }
+  } else {
+    for (let i = 0; i < arrB.length; i++) {
+      arrMerged.push(arrA[i], arrB[i])
+    }
+  }
+
+  return arrMerged.join("")
 }
