fb599c99b57b49c1af55a287d6f320c8a466f1c2
ferniemann <hello@ferdinand-niemann.eu>
2021-12-23 12:52:03 +0100

    Refactor: refactored zip-strings.js

--- zip-strings.js
+++ zip-strings.js
@@ -2,16 +2,11 @@ function zipStrings(strA, strB) {
   const arrA = strA.split("")
   const arrB = strB.split("")
   const arrMerged = []
+  const length = Math.max(arrA.length, arrB.length)
 
-  if (arrA.length > arrB.length) {
-    for (let i = 0; i < arrA.length; i++) {
-      arrMerged.push(arrA[i], arrB[i])
-    }
-  } else {
-    for (let i = 0; i < arrB.length; i++) {
-      arrMerged.push(arrA[i], arrB[i])
-    }
+  for (let i = 0; i < length; i++) {
+    arrMerged.push(arrA[i], arrB[i])
   }
-
+  console.log(arrMerged)
   return arrMerged.join("")
 }
